32c9c0fef81075cc344c8afeae49c608a34c6fe7
hiro <yassirelyakine@gmail.com>
2023-06-12 12:27:45 +0100

    update 400

--- day04/400/ft_iterative_factorial.c
+++ day04/400/ft_iterative_factorial.c
@@ -13,7 +13,8 @@ int ft_iterative_factorial(int nb)
   {
     return 1;
   }
-  if (nb < 0)
+  // any integer larger than 12 produces inaccurate results
+  if (nb < 0 || nb > 12)
   {
     return 0;
   }
