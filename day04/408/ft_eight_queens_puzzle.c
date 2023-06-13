f04c1aac25b5326a81d320dc3cfdd2245c72acb0
hiro <yassirelyakine@gmail.com>
2023-06-13 17:04:14 +0100

    408 -> checker chess board simulation ... OK row checking ... OK column checking ... OK

--- day04/408/ft_eight_queens_puzzle.c
+++ day04/408/ft_eight_queens_puzzle.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int ft_eight_queens_puzzle(void);
-int checker(int board[][8], int i, int j, int row_occupied, int column_occupied);
+int checker(int board[][8], int i, int j, int row_occupied, int column_occupied, int k);
 int check(int board[][8]);
 
 int main(void)
@@ -9,7 +9,24 @@ int main(void)
   printf("%d", ft_eight_queens_puzzle());
 }
 
-int checker(int board[][8], int i, int j, int row_occupied, int column_occupied)
+int ft_eight_queens_puzzle(void)
+{
+  int board[8][8] =
+  {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1}
+  };
+
+  return check(board);
+}
+
+int checker(int board[][8], int i, int j, int row_occupied, int column_occupied, int k)
 {
   if (i > 7)
   {
@@ -19,38 +36,30 @@ int checker(int board[][8], int i, int j, int row_occupied, int column_occupied)
   // check rows
   while (j < 8)
   {
-    if (board[i][j])
-    {
-      row_occupied++;
-    }
+    k = 0;
+    column_occupied = 0;
+    row_occupied += board[i][j];
     if (row_occupied > 1)
     {
       return 0;
     }
+    // check columns
+    while (k < 8)
+    {
+      column_occupied += board[k][j];
+      if (column_occupied > 1)
+      {
+        return 0;
+      }
+      k++;
+    }
     j++;
   }
 
-  checker(board, i + 1, 0, 0, 0);
+  checker(board, i + 1, 0, 0, 0, 0);
 }
 
 int check(int board[][8])
 {
-  return checker(board, 0, 0, 0, 0);
-}
-
-int ft_eight_queens_puzzle(void)
-{
-  int board[8][8] =
-  {
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0}
-  };
-
-  return check(board);
+  return checker(board, 0, 0, 0, 0, 0);
 }
