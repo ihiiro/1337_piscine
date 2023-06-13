be6cba730f83533d820527a04ff2badab6a6b5d2
hiro <yassirelyakine@gmail.com>
2023-06-13 15:11:01 +0100

    408 -> checker replace outer while loop with recursive call fix bug that discounted the last row implement check() as abstraction for checker(...)

--- day04/408/ft_eight_queens_puzzle.c
+++ day04/408/ft_eight_queens_puzzle.c
@@ -1,50 +1,49 @@
 #include <stdio.h>
 
 int ft_eight_queens_puzzle(void);
-int checker(int board[][8]);
+int checker(int board[][8], int i, int j, int row_occupied, int column_occupied);
+int check(int board[][8]);
 
 int main(void)
 {
   printf("%d", ft_eight_queens_puzzle());
 }
 
-int checker(int board[][8])
+int checker(int board[][8], int i, int j, int row_occupied, int column_occupied)
 {
-  // check rows
-  int i = 0;
-  int j = 0;
-  int row_occupied = 0;
-  int column_occupied = 0;
-  while (i < 8)
+  if (i > 7)
   {
-    j = 0;
-    row_occupied = 0;
-    while (j < 8)
-    {
-      if (board[i][j])
-      {
-        row_occupied++;
-      }
-      if (row_occupied > 1)
-      {
-        return 0;
-      }
-      j++;
-    }
-    i++;
+    return 1;
   }
 
-  return 1;
+  // check rows
+  while (j < 8)
+  {
+    if (board[i][j])
+    {
+      row_occupied++;
+    }
+    if (row_occupied > 1)
+    {
+      return 0;
+    }
+    j++;
+  }
+
+  checker(board, i + 1, 0, 0, 0);
 }
 
-
+int check(int board[][8])
+{
+  return checker(board, 0, 0, 0, 0);
+}
 
 int ft_eight_queens_puzzle(void)
 {
   int board[8][8] =
   {
     {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0},
@@ -53,5 +52,5 @@ int ft_eight_queens_puzzle(void)
     {0, 0, 0, 0, 0, 0, 0, 0}
   };
 
-  return checker(board);
+  return check(board);
 }
