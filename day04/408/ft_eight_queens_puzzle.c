9f0fbd3ea00346398b9908772f8036d334c89f67
hiro <yassirelyakine@gmail.com>
2023-06-21 21:08:40 +0100

    -> update 408 <-
    SRQ
    
    efficient data structure ... SEEMS_OK
    init all queens to a1 ... OK_MAY_REMOVE
    survey() ... ALMOST_OK
     survey_cols() ... SEEMS_OK
     survey_diagonals() ... SEEMS_OK

--- day04/408/ft_eight_queens_puzzle.c
+++ day04/408/ft_eight_queens_puzzle.c
@@ -14,16 +14,16 @@ int survey_diagonals(queen *arr, int index, int i, char col, char row, int flp);
 int main(void)
 {
   queen queens[8];
-  queens[0].col = 'a';
+  queens[0].col = 'c';
   queens[0].row = '1';
-  queens[1].col = 'h';
-  queens[1].row = '3';
-  queens[2].col = 'b';
-  queens[2].row = '1';
+  queens[1].col = 'a';
+  queens[1].row = '4';
+  queens[2].col = 'e';
+  queens[2].row = '8';
   // queens[3].col = 'a';
   // queens[3].col = 'a';
   // init(queens, 7);
-  printf("%d", survey_diagonals(queens, 2, 0, queens[2].col - 1, queens[2].row - 1, 0));
+  printf("%d", survey_diagonals(queens, 2, 0, queens[2].col - 1, queens[2].row - 1, -1));
 }
 
 void init(queen *arr, int index)
@@ -71,7 +71,7 @@ int survey_diagonals(queen *arr, int index, int i, char col, char row, int flp)
   {
     return 0;
   }
-  else if (!flp && (col < 'a' || row < '1'))
+  else if (flp < 0 && (col < 'a' || row < '1'))
   {
     survey_diagonals(arr, index, 0, arr[index].col + 1, arr[index].row - 1, 1);
   }
@@ -79,13 +79,9 @@ int survey_diagonals(queen *arr, int index, int i, char col, char row, int flp)
   {
     return 1;
   }
-  else if (!flp && i == index)
+  else if (i == index)
   {
-    survey_diagonals(arr, index, 0, col - 1, row - 1, flp);
-  }
-  else if (flp && i == index)
-  {
-    survey_diagonals(arr, index, 0, col + 1, row - 1, flp);
+    survey_diagonals(arr, index, 0, col + flp, row - 1, flp);
   }
   else
   {
