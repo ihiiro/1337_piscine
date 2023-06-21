589ec7d371dbdc6aca8231fdfdff27c340d51aaf
hiro <yassirelyakine@gmail.com>
2023-06-21 20:32:41 +0100

    -> update 408 <-
    SRQ
    
    efficient data structure ... SEEMS OK
    init all queens to a1 ... OK (MAY REMOVE)
    survey() ... ALMOST OK

--- day04/408/ft_eight_queens_puzzle.c
+++ day04/408/ft_eight_queens_puzzle.c
@@ -8,11 +8,22 @@ struct queen
 typedef struct queen queen;
 
 void init(queen *arr, int index);
+int survey_cols(queen *arr, int index, int i);
+int survey_diagonals(queen *arr, int index, int i, char col, char row, int flp);
 
 int main(void)
 {
   queen queens[8];
-  init(queens, 7);
+  queens[0].col = 'a';
+  queens[0].row = '1';
+  queens[1].col = 'h';
+  queens[1].row = '3';
+  queens[2].col = 'b';
+  queens[2].row = '1';
+  // queens[3].col = 'a';
+  // queens[3].col = 'a';
+  // init(queens, 7);
+  printf("%d", survey_diagonals(queens, 2, 0, queens[2].col - 1, queens[2].row - 1, 0));
 }
 
 void init(queen *arr, int index)
@@ -26,5 +37,58 @@ void init(queen *arr, int index)
   else
   {
     return;
+  }
+}
+
+int survey_cols(queen *arr, int index, int i)
+{
+  if (!arr || index <= 0 || i < 0 || i > index)
+  {
+    return -1;
+  }
+  else if (i == index)
+  {
+    return 1;
+  }
+  else if (arr[index].col == arr[i].col)
+  {
+    return 0;
+  }
+  // else if (arr[index].row == arr[i].row)
+  // {
+  //   return 0;
+  // }
+  survey_cols(arr, index, i + 1);
+}
+
+int survey_diagonals(queen *arr, int index, int i, char col, char row, int flp)
+{
+  if (!arr || index <= 0 || i < 0 || i > index)
+  {
+    return -1;
+  }
+  else if (arr[i].col == col && arr[i].row == row)
+  {
+    return 0;
+  }
+  else if (!flp && (col < 'a' || row < '1'))
+  {
+    survey_diagonals(arr, index, 0, arr[index].col + 1, arr[index].row - 1, 1);
+  }
+  else if (flp && (col > 'h' || row < '1'))
+  {
+    return 1;
+  }
+  else if (!flp && i == index)
+  {
+    survey_diagonals(arr, index, 0, col - 1, row - 1, flp);
+  }
+  else if (flp && i == index)
+  {
+    survey_diagonals(arr, index, 0, col + 1, row - 1, flp);
+  }
+  else
+  {
+    survey_diagonals(arr, index, i + 1, col, row, flp);
   }
 }
